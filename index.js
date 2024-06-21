3a55f94c5c641530e74df742281e0f5e7314cce8
miskumis <110418086+miskumis@users.noreply.github.com>
2024-06-21 13:58:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ async function delay(ms) {
   const randomAddresses = generateRandomAddresses(100)
   console.log('Generated 100 random addresses:', randomAddresses)
 
-  const amountToSend = 0.001
+  const amountToSend = 0.00001
   let currentKeypairIndex = 0
   const delayBetweenRequests = 5000
 
